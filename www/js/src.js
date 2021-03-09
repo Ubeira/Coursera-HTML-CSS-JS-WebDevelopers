ab0262f64c3df81739900494de5b6c05ff2e000b
vira <ubeiram@gmail.com>
2021-03-09 11:00:43 +0100

    estructura inicial con funciones

---
+++ www/js/src.js
@@ -0,0 +1,20 @@
+function init() {
+  console.log('Iniciado');
+  weather();
+}
+
+function weather() {
+  console.log('datos');
+}
+
+function today() {
+  console.log('hoy');
+}
+
+function tomorrow() {
+  console.log('mañana');
+}
+
+function week() {
+  console.log('semana');
+}
