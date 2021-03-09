8556adc3e7d6be70b10d84991157e4cf8821180f
vira <ubeiram@gmail.com>
2021-03-09 12:15:52 +0100

    array con datos today

--- www/js/src.js
+++ www/js/src.js
@@ -2,7 +2,18 @@ function init() {
   console.log('Iniciado');
   weather();
 }
+//Selecciono el contenedor donde pondré mi información
+const container = document.getElementById('principal');
+
+//Creo un fragmento de documento
+let deNode = document.createDocumentFragment();
+
+//Para acceder a los datos desde cualquier funcion la declaro fuera
 let data;
+
+/**
+ * @returns data: Object with data of Weather:{current,daily,hourly}
+ */
 function weather() {
   console.log('datos');
   /**
@@ -32,8 +43,41 @@ function weather() {
 }
 
 function today() {
-  console.log('hoy');
-  console.log(data.hourly);
+  let todayArray = [];
+  let max = { temp: -100, hour: '' };
+  let min = { temp: 100, hour: '' };
+
+  //  almaceno en un array de arrays el tiempo la temperatura
+  for (let i = 0; i < 23; i++) {
+    todayArray = [
+      ...todayArray,
+      {
+        hours: new Date(data.hourly[i].dt * 1000).getHours(),
+        temp: data.hourly[i].temp,
+      },
+    ];
+    if (max.temp < data.hourly[i].temp) {
+      max.temp = data.hourly[i].temp;
+      max.hour = new Date(data.hourly[i].dt * 1000).getHours();
+    }
+    if (min.temp > data.hourly[i].temp) {
+      min.temp = data.hourly[i].temp;
+      min.hour = new Date(data.hourly[i].dt * 1000).getHours();
+    }
+  }
+  //  data.hourly.map((hour) => {
+  //    todayArray = [
+  //      ...todayArray,
+  //      { hours: new Date(hour.dt * 1000).getHours(), temp: hour.temp },
+  //    ];
+
+  console.log(min, max);
+  console.log(todayArray);
+  //Creo un canvas donde pondré la gráfica
+  const canvas = document.createElement('canvas');
+  updateCanvas(canvas, todayArray);
+  deNode.appendChild(canvas);
+  container.appendChild(deNode);
 }
 
 function tomorrow() {
@@ -44,4 +88,23 @@ function tomorrow() {
 function week() {
   console.log(data.daily);
   console.log('semana');
+}
+
+const canvasPadding = 20;
+
+function updateCanvas(canvas, dataArray) {
+  const wid = window.innerWidth;
+  const hei = canvas.height;
+  const context = canvas.getContext('2d');
+  //Fondo del canvas
+  context.fillStyle = 'lightblue';
+  context.strokeStyle = 'black';
+  context.fillRect(0, 0, wid, hei);
+  context.font = '10p Verdana,sans-serif';
+  context.fillStyle = '#999999';
+
+  context.moveTo(canvasPadding, canvasPadding);
+  context.lineTo(canvasPadding, hei - canvasPadding);
+  context.lineTo(wid - canvasPadding, hei - canvasPadding);
+  fillCanvas(context);
 }
