f736bda2ea6735ed88c034788f1a78dc71664311
vira <ubeiram@gmail.com>
2021-03-09 20:38:14 +0100

    today graph with temperatures

--- www/js/src.js
+++ www/js/src.js
@@ -65,17 +65,9 @@ function today() {
       min.hour = new Date(data.hourly[i].dt * 1000).getHours();
     }
   }
-  //  data.hourly.map((hour) => {
-  //    todayArray = [
-  //      ...todayArray,
-  //      { hours: new Date(hour.dt * 1000).getHours(), temp: hour.temp },
-  //    ];
-
-  console.log(min, max);
-  console.log(todayArray);
   //Creo un canvas donde pondré la gráfica
   const canvas = document.createElement('canvas');
-  updateCanvas(canvas, todayArray);
+  updateCanvas(canvas, todayArray, max, min);
   deNode.appendChild(canvas);
   container.appendChild(deNode);
 }
@@ -90,12 +82,14 @@ function week() {
   console.log('semana');
 }
 
-const canvasPadding = 20;
+const canvasPadding = 25;
+let wid;
+let hei;
 
-function updateCanvas(canvas, dataArray) {
-  const wid = window.innerWidth;
-  const hei = canvas.height;
+function updateCanvas(canvas, dataArray, max, min) {
   const context = canvas.getContext('2d');
+  wid = canvas.width;
+  hei = canvas.height;
   //Fondo del canvas
   context.fillStyle = 'lightblue';
   context.strokeStyle = 'black';
@@ -106,5 +100,90 @@ function updateCanvas(canvas, dataArray) {
   context.moveTo(canvasPadding, canvasPadding);
   context.lineTo(canvasPadding, hei - canvasPadding);
   context.lineTo(wid - canvasPadding, hei - canvasPadding);
-  fillCanvas(context);
+  fillCanvas(context, max, min, hei, wid);
+  createBars(context, dataArray, max, min);
+}
+let graphWidth;
+let graphHeight;
+function fillCanvas(context, max, min, hei, wid) {
+  //EjeY
+  const yData = { min: min.temp - 2, max: max.temp + 2 };
+  let steps = 6; //Math.ceil(yData.max - yData.min);
+  const startY = canvasPadding;
+  const endY = hei - canvasPadding;
+  graphHeight = endY - startY;
+  let currentY;
+  let rangeLength = yData.max - yData.min;
+  let stepSize = rangeLength / steps;
+
+  context.textAlign = 'left';
+  for (let i = 0; i < steps; i++) {
+    currentY = startY + (i / steps) * graphHeight;
+    console.log(yData.min + stepSize * (steps - i));
+    context.moveTo(wid - canvasPadding, currentY);
+    context.lineTo(canvasPadding, currentY);
+    context.fillText(
+      (yData.min + stepSize * (steps - i)).toFixed(1),
+      0,
+      currentY + 4
+    );
+  }
+  currentY = startY + graphHeight;
+  context.moveTo(canvasPadding, currentY);
+  context.lineTo(canvasPadding / 2, currentY);
+  //  context.fillText(yData.min, 0, currentY - 3);
+
+  //EjeX
+  const xData = { min: min.hour - 1, max: max.hour + 1 };
+  steps = 10; // Math.ceil(xData.max - xData.min);
+  const startX = canvasPadding;
+  const endX = wid - canvasPadding;
+  graphWidth = endX - startX;
+  let currentX;
+  rangeLength = xData.max - xData.min;
+  stepSize = rangeLength / steps;
+  context.textAlign = 'left';
+
+  for (let i = 0; i < steps; i++) {
+    currentX = startX + (i / steps) * graphWidth;
+    context.moveTo(currentX, startY);
+    context.lineTo(currentX, endY);
+    context.fillText(
+      `${(xData.min + stepSize * i).toFixed(0)} h`,
+      currentX - 6,
+      endY + canvasPadding / 2
+    );
+  }
+  currentX = startX + graphWidth;
+  context.moveTo(currentX, startY);
+  context.lineTo(currentX, endY);
+  //  context.fillText(xData.max, currentX - 3, endY + canvasPadding / 2);
+  context.stroke();
+}
+function createBars(context, dataArray, max, min) {
+  console.log('createBars');
+  console.log(dataArray);
+  const yDataLabel = 'T(ºC)';
+  const xDataLabel = 'hora';
+  const yDataRange = max.temp - min.temp;
+  const xDataRange = max.hour - min.hour; //No estoy seguro de que sea correcto el rango...
+  let yPos;
+  let xPos;
+
+  for (let i = 0; i < dataArray.length; i++) {
+    xPos =
+      canvasPadding +
+      ((dataArray[i].hours - min.hour) / xDataRange) * graphWidth;
+    yPos =
+      hei -
+      canvasPadding -
+      ((dataArray[i].temp - min.temp) / yDataRange) * graphHeight;
+    console.log(dataArray[i], min);
+    if (dataArray[i].temp < 10) {
+      context.fillStyle = 'blue';
+    } else {
+      context.fillStyle = 'red';
+    }
+    context.fillRect(xPos - 4, yPos - 4, 8, 8);
+  }
 }
